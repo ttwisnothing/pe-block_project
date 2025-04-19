0548bf4f62f95804eb0e9d0ac017d4b8246a9a00
ttwisnothing <theethawat.chai@gmail.com>
2025-04-19 16:54:09 +0700

    เอาขึ้น server กลาง

--- server/config/db.js
+++ server/config/db.js
@@ -7,7 +7,7 @@ dotenv.config(); // โหลดค่าจากไฟล์ .env
 const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 })
 
